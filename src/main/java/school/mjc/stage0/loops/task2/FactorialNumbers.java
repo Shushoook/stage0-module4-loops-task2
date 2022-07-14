3aa3339970119c4e3606790bbd92e27d31cee99c
Shushoook <shushoook666@gmail.com>
2022-07-14 16:50:16 +0400

    stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+        int factorialRaw = 0;
+        while (factorialRaw <= printToInclusive) {
+            System.out.println(factorial(factorialRaw));
+            factorialRaw++;
+        }
+    }
+
+    public int factorial(int printToInclusive) {
+        if (printToInclusive==0){
+            return 1;
+        }
+        return printToInclusive * factorial(printToInclusive - 1);
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        if (printToInclusive == 0) {
+            System.out.print("");
+        } else {
+            System.out.println(2);
+            int count = 2;
+            boolean check = false;
+            while (count <= printToInclusive) {
+                int devider = 2;
+                while (devider <= count) {
+                    if (count % devider == 0) {
+                        devider++;
+                        break;
+                    }
+                    if (++devider == count) {
+                        System.out.println(count);
+                        break;
+                    }
+                    //    break;
+                }
+                count++;
+            }
+        }
     }
 }
